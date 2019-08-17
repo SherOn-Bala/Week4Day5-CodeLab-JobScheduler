b5cc392ca4837dd1b1da761db689208709c4ad5f
SherOn-Bala <jude.balasingam@mobileappscompany.us>
2019-08-17 13:00:40 -0400

    added log statements

--- app/src/main/java/ca/judacribz/week4day5_codelab_jobscheduler/AsyncJobService.java
+++ app/src/main/java/ca/judacribz/week4day5_codelab_jobscheduler/AsyncJobService.java
@@ -5,20 +5,28 @@ import android.app.job.JobService;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class AsyncJobService extends JobService implements WaitTask.OnTaskCompleteCallback {
 
     private static final String TAG = AsyncJobService.class.getSimpleName();
 
     private WaitTask waitTask;
     private JobParameters jobParameters;
+    private boolean reschedule = false;
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         this.jobParameters = jobParameters;
 
-        Log.d(TAG, "onStopJob: STARTING TASK");
         waitTask = new WaitTask(this);
         waitTask.execute();
+
+        Log.d(TAG, String.format(
+                Locale.US,
+                "onStartJob: %s",
+                reschedule ? "RESTARTING TASK" : "STARTING TASK"
+        ));
 
         return true;
     }
@@ -29,15 +37,17 @@ public class AsyncJobService extends JobService implements WaitTask.OnTaskComple
             waitTask.cancel(true);
         }
 
+        reschedule = true;
         Toast.makeText(getApplicationContext(), "Job Failed", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStopJob: TASK STOPPED...RESCHEDULING");
+        Log.d(TAG, "onStopJob: TASK STOPPED");
+
         return true;
     }
 
     @Override
     public void onTaskCompleted(boolean success) {
         jobFinished(jobParameters, !success);
-
+        reschedule = false;
         Log.d(TAG, "onStopJob: TASK COMPLETE");
     }
 }
