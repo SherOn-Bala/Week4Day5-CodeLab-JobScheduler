fe2406bfd46ae74c882fd20ad14a66d6c68b137c
SherOn-Bala <jude.balasingam@mobileappscompany.us>
2019-08-17 03:57:39 -0400

    finished codelab

---
+++ README.md
@@ -0,0 +1,7 @@
+## Screenshots ##
+<p float="left">
+  <img src="/screenshots/1.png" width="200" />
+</p>
+
+## Code ##
+### [JobScheduler CodeLab](https://codelabs.developers.google.com/codelabs/android-training-job-scheduler/index.html?index=..%2F..%2Fandroid-training#0) ###
---
+++ app/src/main/java/ca/judacribz/week4day5_codelab_jobscheduler/MainActivity.java
@@ -0,0 +1,167 @@
+package ca.judacribz.week4day5_codelab_jobscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int JOB_ID = 0;
+    private static final int JOB_FINISH_ID = 1;
+    private JobScheduler mScheduler;
+
+    private RadioGroup networkOptions;
+
+    //Switches for setting job options
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+
+    //Override deadline seekbar
+    private SeekBar mSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        networkOptions = findViewById(R.id.networkOptions);
+        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mSeekBar = findViewById(R.id.seekBar);
+
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (i > 0){
+                    seekBarProgress.setText(String.format(getString(R.string.s), i));
+                }else {
+                    seekBarProgress.setText(R.string.not_set);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void scheduleJob(View view) {
+
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        int seekBarInteger = mSeekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+        switch (selectedNetworkID) {
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(
+                getPackageName(),
+                NotificationJobService.class.getName()
+        );
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        boolean constraintSet =
+                (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE) ||
+                        mDeviceChargingSwitch.isChecked() ||
+                        mDeviceIdleSwitch.isChecked() ||
+                        seekBarSet;
+
+        if (constraintSet) {
+            //Schedule the job and notify the user
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+        } else {
+            Toast.makeText(this, "Please set at least one constraint",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cancelJobs(View view) {
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void jobFinished(View view) {
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        int seekBarInteger = mSeekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+        switch (selectedNetworkID) {
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(
+                getPackageName(),
+                AsyncTaskJobService.class.getName()
+        );
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_FINISH_ID, serviceName);
+        builder
+                .setRequiredNetworkType(selectedNetworkOption)
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        boolean constraintSet =
+                (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE) ||
+                        mDeviceChargingSwitch.isChecked() ||
+                        mDeviceIdleSwitch.isChecked() ||
+                        seekBarSet;
+
+        if (constraintSet) {
+            //Schedule the job and notify the user
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+        } else {
+            Toast.makeText(this, "Please set at least one constraint",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
